fe0014aa4fbdd08be4c5f8306b812c8b0d77df51
Bagaev Dmitri <bvdmitri@gmail.com>
2014-09-17 20:13:19 +0400

    vdjtools added in dependices

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,15 @@
 package controllers;
 
+import com.antigenomics.vdjtools.diversity.Diversity;
 import play.*;
 import play.mvc.*;
 
 import views.html.*;
 
 public class Application extends Controller {
-
     public static Result index() {
+        Diversity f = new com.antigenomics.vdjtools.diversity.Diversity(1,1,1,false);
+        System.out.println( f.toString());
         return ok(index.render("Your new application is ready."));
     }
-
 }
--- build.sbt
+++ build.sbt
@@ -1,4 +1,4 @@
-name := """play-java"""
+name := """vdjviz"""
 
 version := "1.0-SNAPSHOT"
 
@@ -6,7 +6,12 @@ lazy val root = (project in file(".")).enablePlugins(PlayJava)
 
 scalaVersion := "2.11.1"
 
+resolvers += (
+  "Local Maven Repository" at "file:///"+Path.userHome.absolutePath+"/.m2/repository"
+  )
+
 libraryDependencies ++= Seq(
+  "com.antigenomics" % "vdjtools" % "1.0-SNAPSHOT",
   javaJdbc,
   javaEbean,
   cache,
