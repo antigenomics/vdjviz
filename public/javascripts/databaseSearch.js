49fc176616362791f1d0b62eb8bff51abc26f095
bvdmitri <bvdmitri@gmail.com>
2014-12-17 18:51:52 +0300

    validation check

--- public/javascripts/databaseSearch.js
+++ public/javascripts/databaseSearch.js
@@ -12,25 +12,12 @@
                 $scope.isAdditionalInfo = isAdditionalInfo;
                 $scope.searchInput = searchInput;
                 $scope.isMainInfo = isMainInfo;
+                $scope.isValid = isValid;
                 $scope.searchResult = [];
                 $scope.additionalInfo = {
                     data: null,
                     show: false
                 };
-
-                //private variables
-                var locationAttr = $location.search();
-                var input = locationAttr.input;
-                var info = locationAttr.info;
-
-                //Initializing
-                if (typeof input == 'string') {
-                    $scope.input = locationAttr.input;
-                    searchInput(info);
-                } else {
-                    $scope.input = 'CASSLAPGATNEKLFF';
-                }
-
                 $rootScope.$on('$locationChangeSuccess', function() {
                     var newInfo = $location.search().info;
                     if (typeof newInfo === 'undefined') {
@@ -38,16 +25,31 @@
                     }
                 });
 
+                //Initializing
+                var input = $location.search().input;
+                if (typeof input == 'string') {
+                    $scope.input = input;
+                    searchInput();
+                } else {
+                    $scope.input = 'CASSLAPGATNEKLFF';
+                }
+
+                function isValid() {
+                    var regexp = /^[FLSYCWPHQRIMTNKVADEG]+$/;
+                    return regexp.test($scope.input);
+                }
+
                 function showAdditionalInfo(result) {
                     $scope.additionalInfo.data = result;
                     $location.search('info', result.hash);
                     $scope.additionalInfo.show = true;
                 }
 
-                function searchInput(info) {
+                function searchInput() {
                     $http.post('/database/api/search', { input: $scope.input })
                         .success(function(data) {
                             $location.search('input', $scope.input);
+                            var info = $location.search().info;
                             $scope.searchResult = data;
                             angular.forEach(data, function(result) {
                                 if (result.hash == info) {
